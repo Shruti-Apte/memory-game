67e9d2a6b935a6f0369e224b542dabb073ae6036
Shruti Apte <shruti@saeloun.com>
2025-02-19 11:41:07 +0530

    fixed audio not playing bug

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { Howler } from "howler";
 import { SharedProvider } from "./Context/SharedContext";
-
 import AppWrapper from "./Components/AppWrapper";
 import "../src/assets/css/App.css";
-import { ErrorBoundary } from 'react-error-boundary';
 
-const ErrorFallback = ({ error}) => {
-return (
+const ErrorFallback = ({ error }) => {
+  return (
     <div className="theme h-screen w-full flex flex-col items-center justify-center">
-      <h2 className='text-3xl text-center text-red'>Oops! An error occurred </h2>
+      <h2 className="text-3xl text-center text-red">
+        Oops! An error occurred{" "}
+      </h2>
       <h2>{error.message}</h2>
     </div>
   );
 };
 
 function App() {
+  document.addEventListener("click", () => {
+    if (Howler.ctx.state === "suspended") {
+      Howler.ctx.resume();
+    }
+  });
   return (
-      <SharedProvider>
-        <ErrorBoundary FallbackComponent={ErrorFallback}>
-         <AppWrapper />
-        </ErrorBoundary>
-      </SharedProvider>
+    <SharedProvider>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <AppWrapper />
+      </ErrorBoundary>
+    </SharedProvider>
   );
 }
 
--- src/Components/sounds.js
+++ src/Components/sounds.js
@@ -1,20 +1,20 @@
-import {Howl, Howler} from 'howler';
+import { Howl, Howler } from 'howler';
 
 const soundFiles = [
-    "/assets/sounds/twinkle.mp3",
-    "/assets/sounds/squeak.mp3",
-    "/assets/sounds/splash.mp3",
-    "/assets/sounds/jump.mp3",
-    "/assets/sounds/bell.mp3",
-    "/assets/sounds/snap.mp3",
-    "/assets/sounds/pop.mp3",
-    "/assets/sounds/failed.mp3",
-    "/assets/sounds/tada.mp3",
-    "/assets/sounds/startMusic.mp3",
-    "/assets/sounds/countdown.wav",
+    process.env.PUBLIC_URL + "/sounds/twinkle.mp3",
+    process.env.PUBLIC_URL + "/sounds/squeak.mp3",
+    process.env.PUBLIC_URL + "/sounds/splash.mp3",
+    process.env.PUBLIC_URL + "/sounds/jump.mp3",
+    process.env.PUBLIC_URL + "/sounds/bell.mp3",
+    process.env.PUBLIC_URL + "/sounds/snap.mp3",
+    process.env.PUBLIC_URL + "/sounds/pop.mp3",
+    process.env.PUBLIC_URL + "/sounds/failed.mp3",
+    process.env.PUBLIC_URL + "/sounds/tada.mp3",
+    process.env.PUBLIC_URL + "/sounds/startMusic.mp3",
+    process.env.PUBLIC_URL + "/sounds/countdown.wav",
   ];
   
   const sounds = soundFiles.map((src) => new Howl({ src: [src] }));
-  Howler.volume(2.0);
+  Howler.volume(1.0);
   
   export default sounds;
